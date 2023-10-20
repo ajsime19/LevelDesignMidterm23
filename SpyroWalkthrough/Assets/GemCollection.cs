2fb89827d24e8e1ccedb7710a80139a0c6f3f0b2
ajsime <148512289+ajsime19@users.noreply.github.com>
2023-10-20 01:09:14 -0500

    added gems and functionality

--- SpyroWalkthrough/Assets/GemCollection.cs
+++ SpyroWalkthrough/Assets/GemCollection.cs
@@ -6,8 +6,10 @@ using TMPro;
 public class GemCollection : MonoBehaviour
 {
     public int gemTotal = 0;
+    public int dragonTotal = 0;
 
     public TextMeshProUGUI gemCounter;
+    public TextMeshProUGUI dragonCounter;
 
     // Start is called before the first frame update
     void Start()
@@ -28,6 +30,16 @@ public class GemCollection : MonoBehaviour
             gemTotal = gemTotal + other.gameObject.GetComponent<Gem>().gemValue;
             gemCounter.text = gemTotal.ToString() + "/100";
             Debug.Log(gemTotal);
+
+            Destroy(other.gameObject);
+        }
+
+
+        if(other.tag == "Dragon")
+        {
+            dragonTotal = dragonTotal + 1;
+            dragonCounter.text = dragonTotal.ToString() + "/6";
+            Debug.Log(dragonTotal);
 
             Destroy(other.gameObject);
         }
